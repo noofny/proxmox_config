6a1ae8b25e7d333ab6900033293dd0f564f1741d
noofny <noofny@gmail.com>
2022-02-22 21:20:17 +1100

    Update fix_ssh.sh

--- fix_ssh.sh
+++ fix_ssh.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+ssh-keygen -f "/root/.ssh/known_hosts" -R pve-1
+ssh-keygen -f "/root/.ssh/known_hosts" -R pve-7
+ssh-keygen -f "/root/.ssh/known_hosts" -R pve-8
+ssh-keygen -f "/root/.ssh/known_hosts" -R pve-1.home
+ssh-keygen -f "/root/.ssh/known_hosts" -R pve-7.home
+ssh-keygen -f "/root/.ssh/known_hosts" -R pve-8.home
+ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.0.5
+ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.0.7
+ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.0.8
+echo "Hit ENTER to continue (1/2)"
+read
+
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-1
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-7
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-8
-
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-1.home
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-7.home
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-8.home
-
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R 192.168.0.5
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R 192.168.0.7
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R 192.168.0.8
+echo "Hit ENTER to continue (2/2)"
+read
 
 /usr/bin/ssh -e none -o 'HostKeyAlias=pve-1' root@192.168.0.5 /bin/true
 /usr/bin/ssh -e none -o 'HostKeyAlias=pve-7' root@192.168.0.7 /bin/true
