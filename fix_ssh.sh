1b259d2d0c0f0af1e0316d2b17399ffbad505d10
noofny <noofny@gmail.com>
2024-01-05 07:59:17 +1100

    Update fix_ssh.sh

--- fix_ssh.sh
+++ fix_ssh.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
 ssh-keygen -f "/root/.ssh/known_hosts" -R pve-1
-ssh-keygen -f "/root/.ssh/known_hosts" -R pve-7
-ssh-keygen -f "/root/.ssh/known_hosts" -R pve-8
+ssh-keygen -f "/root/.ssh/known_hosts" -R pve-2
+ssh-keygen -f "/root/.ssh/known_hosts" -R pve-3
 ssh-keygen -f "/root/.ssh/known_hosts" -R pve-1.home
-ssh-keygen -f "/root/.ssh/known_hosts" -R pve-7.home
-ssh-keygen -f "/root/.ssh/known_hosts" -R pve-8.home
+ssh-keygen -f "/root/.ssh/known_hosts" -R pve-2.home
+ssh-keygen -f "/root/.ssh/known_hosts" -R pve-3.home
 ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.0.5
+ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.0.6
 ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.0.7
-ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.0.8
 echo "Hit ENTER to continue (1/3)"
 read
 
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-1
-ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-7
-ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-8
+ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-2
+ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-3
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-1.home
-ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-7.home
-ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-8.home
+ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-2.home
+ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R pve-3.home
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R 192.168.0.5
+ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R 192.168.0.6
 ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R 192.168.0.7
-ssh-keygen -f "/etc/ssh/ssh_known_hosts" -R 192.168.0.8
 echo "Hit ENTER to continue (2/3)"
 read
 
 ssh-keygen -f "/etc/pve/priv/known_hosts" -R pve-1
-ssh-keygen -f "/etc/pve/priv/known_hosts" -R pve-7
-ssh-keygen -f "/etc/pve/priv/known_hosts" -R pve-8
+ssh-keygen -f "/etc/pve/priv/known_hosts" -R pve-2
+ssh-keygen -f "/etc/pve/priv/known_hosts" -R pve-3
 ssh-keygen -f "/etc/pve/priv/known_hosts" -R pve-1.home
-ssh-keygen -f "/etc/pve/priv/known_hosts" -R pve-7.home
-ssh-keygen -f "/etc/pve/priv/known_hosts" -R pve-8.home
+ssh-keygen -f "/etc/pve/priv/known_hosts" -R pve-2.home
+ssh-keygen -f "/etc/pve/priv/known_hosts" -R pve-3.home
 ssh-keygen -f "/etc/pve/priv/known_hosts" -R 192.168.0.5
+ssh-keygen -f "/etc/pve/priv/known_hosts" -R 192.168.0.6
 ssh-keygen -f "/etc/pve/priv/known_hosts" -R 192.168.0.7
-ssh-keygen -f "/etc/pve/priv/known_hosts" -R 192.168.0.8
 echo "Hit ENTER to continue (3/3)"
 read
 
 /usr/bin/ssh -e none -o 'HostKeyAlias=pve-1' root@192.168.0.5 /bin/true
-/usr/bin/ssh -e none -o 'HostKeyAlias=pve-7' root@192.168.0.7 /bin/true
-/usr/bin/ssh -e none -o 'HostKeyAlias=pve-8' root@192.168.0.8 /bin/true
+/usr/bin/ssh -e none -o 'HostKeyAlias=pve-2' root@192.168.0.6 /bin/true
+/usr/bin/ssh -e none -o 'HostKeyAlias=pve-3' root@192.168.0.7 /bin/true
 /usr/bin/ssh -e none -o 'HostKeyAlias=pve-1.home' root@192.168.0.5 /bin/true
-/usr/bin/ssh -e none -o 'HostKeyAlias=pve-7.home' root@192.168.0.7 /bin/true
-/usr/bin/ssh -e none -o 'HostKeyAlias=pve-8.home' root@192.168.0.8 /bin/true
+/usr/bin/ssh -e none -o 'HostKeyAlias=pve-2.home' root@192.168.0.6 /bin/true
+/usr/bin/ssh -e none -o 'HostKeyAlias=pve-3.home' root@192.168.0.7 /bin/true
 
 echo "Done!"
